a8dff34ac3e1716447816cbd56cf2d01563ff626
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2023-11-13 17:08:20 +0100

    set config

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-    stylistic: true
-})
+const stefanobartoletti = antfu({
+    stylistic: true,
+    vue: true,
+    typescript: true,
+},{
+    // Vue rules
+    files: ['**/*.vue'],
+    rules: {
+      'vue/max-attributes-per-line': ['error', {
+        singleline: { max: 10 },
+        multiline: { max: 1 },
+      }],
+      'vue/singleline-html-element-content-newline': 0,
+      'vue/html-self-closing': ['warn', {
+        html: {
+          void: 'always',
+          normal: 'never',
+        },
+      }],
+      'vue/block-order': ['error', {
+        order: ['template', 'script', 'style'],
+      }],
+    },
+  },
+  {
+    // General rules
+    rules: {
+      curly: ['error', 'all'],
+    },
+  },
+
+)
+
+export default stefanobartoletti
