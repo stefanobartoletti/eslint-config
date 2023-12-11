e531097ddaf957c479c2ff09bdacb1d42174eea4
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2023-12-11 16:03:55 +0100

    feat: add css & html support

--- rules/antfuOptions.js
+++ rules/antfuOptions.js
@@ -2,6 +2,10 @@ const antfuOptions = {
   stylistic: true,
   // vue: true, // autodetected by @antfu/eslint-config, no need to set a default
   // typescript: true, // autodetected by @antfu/eslint-config, no need to set a default
+  formatters: {
+    css: true,
+    html: true,
+  },
 }
 
 export default antfuOptions
