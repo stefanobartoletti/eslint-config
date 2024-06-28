1376965ca85085b228dd25b2952727b82c9bd79d
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2024-06-28 16:39:55 +0200

    remove nuxt config package

--- rules/sbConfigBase.js
+++ rules/sbConfigBase.js
@@ -3,10 +3,10 @@ const sbConfigBase = [
   {
     name: 'stefanobartoletti/base',
     rules: {
-      'curly': ['error', 'all'],
-      'style/function-call-spacing': ['error', 'never'],
-      'node/prefer-global/process': 'off',
       'antfu/top-level-function': 'off',
+      'curly': ['error', 'all'],
+      'node/prefer-global/process': 'off',
+      'style/function-call-spacing': ['error', 'never'],
     },
   },
 
