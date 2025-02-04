f272ff4021095a2e7f7671343c1d6a653cac5c60
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2025-02-04 12:48:14 +0100

    feat!: move eslint-plugin-tailwindcss to peerDependencies

--- rules/sbConfigTailwind.js
+++ rules/sbConfigTailwind.js
@@ -1,5 +1,14 @@
-import tailwind from 'eslint-plugin-tailwindcss'
+/* eslint-disable import/no-mutable-exports */
+/* eslint-disable antfu/no-top-level-await */
 
-const sbConfigTailwind = tailwind.configs['flat/recommended']
+let sbConfigTailwind = {}
+
+try {
+  const tailwind = await import('eslint-plugin-tailwindcss')
+  sbConfigTailwind = tailwind.default.configs['flat/recommended']
+}
+catch {
+  console.warn('eslint-plugin-tailwindcss not found, skipping Tailwind rules')
+}
 
 export default sbConfigTailwind
