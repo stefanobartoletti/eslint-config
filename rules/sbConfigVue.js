0988352505366a40f7e683c715ed134d637658a6
Stefano Bartoletti <36746429+stefanobartoletti@users.noreply.github.com>
2026-08-06 14:44:57 +0200

    fix: reduce max attributes per line for singleline in Vue configuration

--- rules/sbConfigVue.js
+++ rules/sbConfigVue.js
@@ -13,7 +13,7 @@ const sbConfigVue = [
         },
       }],
       'vue/max-attributes-per-line': ['error', {
-        singleline: { max: 6 },
+        singleline: { max: 4 },
         multiline: { max: 1 },
       }],
       'vue/multi-word-component-names': 'warn',
